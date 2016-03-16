95f27cdc7cea4e03c1d410d88e93a7c3af3ef5e3
alexclp <alexandru.clapa@gmail.com>
2016-03-16 16:39:03 +0000

    FB and LinkedIn frameworks. Initial commit.

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'ProjaBox' do
+
+use_frameworks!
+
+pod 'Bolts'
+pod 'FBSDKCoreKit'
+pod 'FBSDKShareKit'
+pod 'FBSDKLoginKit'
+
+end
+
+target 'ProjaBoxTests' do
+
+end
+
+target 'ProjaBoxUITests' do
+
+end
+
---
+++ ProjaBox/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ProjaBox
+//
+//  Created by Alexandru Clapa on 16/03/2016.
+//  Copyright © 2016 Alexandru Clapa. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController {
+	
+	@IBOutlet weak var facebookButton: FBSDKLoginButton?
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+}
+
