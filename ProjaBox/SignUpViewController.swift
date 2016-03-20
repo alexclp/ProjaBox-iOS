c91fd5e3307295aef2f093f32648c17fb0cbbf93
alexclp <alexandru.clapa@gmail.com>
2016-03-20 13:37:45 +0000

    Sign up screen

---
+++ ProjaBox/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  ProjaBox
+//
+//  Created by Alexandru Clapa on 20/03/2016.
+//  Copyright © 2016 Alexandru Clapa. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.title = "Sign up"
+	}
+	
+}
