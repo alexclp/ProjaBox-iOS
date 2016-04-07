636ae1f5a0e7ae5a63aaa1f761fe274a7b9080e9
alexclp <alexandru.clapa@gmail.com>
2016-04-08 02:21:45 +0300

    Project organisation + Helper for sharing to be implemented

---
+++ ProjaBox/SharingHelper.swift
@@ -0,0 +1,11 @@
+//
+//  SharingHelper.swift
+//  ProjaBox
+//
+//  Created by Alexandru Clapa on 08/04/2016.
+//  Copyright © 2016 Alexandru Clapa. All rights reserved.
+//
+
+class SharingHelper: NSObject {
+//	TODO: Static method for sharing stuff
+}
