9ef94fa2aebe596c3529b83d93a7c697be6f829e
alexclp <alexandru.clapa@gmail.com>
2016-04-29 23:20:02 +0100

    comments cell

---
+++ ProjaBox/CommentTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  CommentTableViewCell.swift
+//  ProjaBox
+//
+//  Created by Alexandru Clapa on 29/04/2016.
+//  Copyright © 2016 Alexandru Clapa. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+	@IBOutlet weak var timeLabel: UILabel?
+	@IBOutlet weak var nameLabel: UILabel?
+	@IBOutlet weak var positionLabel: UILabel?
+	@IBOutlet weak var commentLabel: UILabel?
+	@IBOutlet weak var profileImageView: UIImageView?
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+	
+	override func layoutSubviews() {
+		imageSetup()
+	}
+	
+	func imageSetup() {
+		profileImageView?.layer.cornerRadius = (profileImageView?.frame.size.width)!/2
+		profileImageView?.clipsToBounds = true
+		profileImageView?.contentMode = .ScaleAspectFit
+		profileImageView?.backgroundColor = UIColor.whiteColor()
+	}
+
+}
---
+++ ProjaBox/PostDetailsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PostDetailsViewController.swift
+//  ProjaBox
+//
+//  Created by Alexandru Clapa on 28/04/2016.
+//  Copyright © 2016 Alexandru Clapa. All rights reserved.
+//
+
+import UIKit
+
+class PostDetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	
+	@IBOutlet weak var commentsTableView: UITableView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+	// MARK: Table View Data Source
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		let rows = 0
+		
+		return rows
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("commentCell")
+		
+		return cell!
+	}
+	
+	// MARK: Table View Delegate
+	
+	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		tableView.deselectRowAtIndexPath(indexPath, animated: true)
+	}
+}
