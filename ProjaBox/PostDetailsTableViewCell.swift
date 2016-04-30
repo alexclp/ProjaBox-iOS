a52761af651360857304fc434a5a51e5fc7576d1
alexclp <alexandru.clapa@gmail.com>
2016-04-30 01:12:06 +0100

    Part of the details cell

---
+++ ProjaBox/PostDetailsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostDetailsTableViewCell.swift
+//  ProjaBox
+//
+//  Created by Alexandru Clapa on 30/04/2016.
+//  Copyright © 2016 Alexandru Clapa. All rights reserved.
+//
+
+import UIKit
+
+class PostDetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
