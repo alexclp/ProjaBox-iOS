d577d55a1eb3c39ec5dfb81c11d1c9fadf0e2b88
alexclp <alexandru.clapa@gmail.com>
2016-06-04 00:50:35 +0100

    Method to create a post

--- ProjaBox/NewsFeedHelper.swift
+++ ProjaBox/NewsFeedHelper.swift
@@ -11,25 +11,44 @@ import UIKit
 import Alamofire
 
 class NewsFeedHelper: NSObject {
-
-	class func getNewsFeed() {
+	
+	private class func createAuthHeadersForURL(URL: NSURL, _ httpMethod: String) -> NSURLRequest {
 		let userData = NSUserDefaults.standardUserDefaults().objectForKey("userData")
 		let userId = userData!["userId"] as! Int
 		let deviceId = userData!["deviceId"] as! Int
 		let token = userData!["token"] as! String
 		
+		let mutableURLRequest = NSMutableURLRequest(URL: URL)
+		mutableURLRequest.setValue(String(userId), forHTTPHeaderField: "Z-UserId")
+		mutableURLRequest.setValue(String(deviceId), forHTTPHeaderField: "Z-DeviceId")
+		mutableURLRequest.setValue(token, forHTTPHeaderField: "Z-Token")
+		mutableURLRequest.HTTPMethod = httpMethod
+		
+		return mutableURLRequest
+	}
+
+	class func getNewsFeed() {
+		let userData = NSUserDefaults.standardUserDefaults().objectForKey("userData")
+		let userId = userData!["userId"] as! Int
 		let urlString = "http://139.59.161.63:8080/projabox-webapp/api/rest/v1/users/\(userId)/feed"
 		if let url = NSURL(string: urlString) {
-			let URLRequest = NSMutableURLRequest(URL: url)
-			URLRequest.setValue(String(userId), forHTTPHeaderField: "Z-UserId")
-			URLRequest.setValue(String(deviceId), forHTTPHeaderField: "Z-DeviceId")
-			URLRequest.setValue(token, forHTTPHeaderField: "Z-Token")
-			
-			URLRequest.HTTPMethod = "GET"
-			Alamofire.request(URLRequest).responseJSON { response in
+			let urlRequest = createAuthHeadersForURL(url, "GET")
+			Alamofire.request(urlRequest).responseJSON { response in
 				print(response.result.error)
 				print(response.result.value)
 			}
+		}
+	}
+	
+	class func createPost(name: String, _content: String, photo: NSData?, video: NSData?) {
+		if let url = NSURL(string: "http://139.59.161.63:8080/projabox-webapp/api/rest/v1/posts") {
+			let urlRequest = createAuthHeadersForURL(url, "POST")
+			
+			let manager = Alamofire.Manager.sharedInstance
+			let request = manager.request(urlRequest)
+			request.responseJSON(completionHandler: { (response) in
+				print(response)
+			})
 		}
 	}
 	
