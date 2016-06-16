0c23af9c4ee95b4ea5acd97551a993506e369e57
alexclp <alexandru.clapa@gmail.com>
2016-06-16 15:57:07 +0300

    classes for filters

---
+++ ProjaBox/TypeFilterViewController.swift
@@ -0,0 +1,36 @@
+
+//
+//  TypeFilterViewController.swift
+//  ProjaBox
+//
+//  Created by Alexandru Clapa on 16/06/2016.
+//  Copyright © 2016 Alexandru Clapa. All rights reserved.
+//
+
+import UIKit
+
+class TypeFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
