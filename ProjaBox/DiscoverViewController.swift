38dba680a815512842a59a798b210ca04d4babc9
alexclp <alexandru.clapa@gmail.com>
2016-06-16 03:11:21 +0300

    Segmented control to select people or projects to search/view

--- ProjaBox/DiscoverViewController.swift
+++ ProjaBox/DiscoverViewController.swift
@@ -16,7 +16,7 @@ class DiscoverViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-		
+		setupSegmentedControl()
 	}
 	
 	override func viewDidAppear(animated: Bool) {
@@ -31,5 +31,28 @@ class DiscoverViewController: UIViewController {
 		
 		searchBar.placeholder = "search..."
 		self.tabBarController!.navigationItem.titleView = searchBar
+	}
+	
+	func setupSegmentedControl() {
+		let segmented = YSSegmentedControl(
+			frame: CGRect(
+				x: 0,
+				y: 0,
+				width: view.frame.size.width,
+				height: 44),
+			titles: [
+				"Projects",
+				"People",
+			],
+			action: {
+				control, index in
+				print ("segmented did pressed \(index)")
+		})
+		//		segmented.delegate = self
+		segmented.appearance.selectedTextColor = UIColor(red: 237/256, green: 84/256, blue: 84/256, alpha: 1)
+		segmented.appearance.bottomLineColor = UIColor(red: 237/256, green: 84/256, blue: 84/256, alpha: 1)
+		segmented.appearance.selectorColor = UIColor(red: 237/256, green: 84/256, blue: 84/256, alpha: 1)
+		
+		view.addSubview(segmented)
 	}
 }
