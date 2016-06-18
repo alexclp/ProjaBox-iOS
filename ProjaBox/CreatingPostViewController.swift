833f6a66757bd6eb2943d8bf1a8253a51f38e138
alexclp <alexandru.clapa@gmail.com>
2016-06-18 14:11:34 +0300

    no more hard coding when posting

--- ProjaBox/CreatingPostViewController.swift
+++ ProjaBox/CreatingPostViewController.swift
@@ -44,8 +44,9 @@ class CreatingPostViewController: UIViewController {
 	}
 	
 	@IBAction func doneButtonPressed() {
+		let textToShare = textView?.text
 		if projectPost == true {
-			ProjectHelper.createPost(projectId, "Post", "Project Post", nil, nil, completionHandler: { (response) in
+			ProjectHelper.createPost(projectId, "Post", textToShare!, nil, nil, completionHandler: { (response) in
 				if response  == false {
 					let alert = UIAlertController(title: "Alert", message: "There was an error while creating the post. Please try again later.", preferredStyle: UIAlertControllerStyle.Alert)
 					alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
@@ -53,7 +54,7 @@ class CreatingPostViewController: UIViewController {
 				}
 			})
 		} else {
-			NewsFeedHelper.createPost("Post", "Created a new post", nil, nil) { (response) in
+			NewsFeedHelper.createPost("Post", textToShare!, nil, nil) { (response) in
 				if response == false {
 					let alert = UIAlertController(title: "Alert", message: "There was an error while creating the post. Please try again later.", preferredStyle: UIAlertControllerStyle.Alert)
 					alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
