c50a64a72b11f75a9ead5f9325ef7514b00e2aa6
alexclp <alexandru.clapa@gmail.com>
2016-06-19 15:12:57 +0300

    fixes in creating post view controller

--- ProjaBox/CreatingPostViewController.swift
+++ ProjaBox/CreatingPostViewController.swift
@@ -116,12 +116,13 @@ extension UIViewController: UITextViewDelegate {
 		toolBar.barStyle = UIBarStyle.Default
 		toolBar.translucent = true
 		toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
-		let globeButton = UIBarButtonItem(title: "Globe", style: UIBarButtonItemStyle.Plain, target: self, action: #selector(UIViewController.globePressed))
+//		let globeButton = UIBarButtonItem(title: "Globe", style: UIBarButtonItemStyle.Plain, target: self, action: #selector(UIViewController.globePressed))
 		let urlButton = UIBarButtonItem(title: "URL", style: UIBarButtonItemStyle.Plain, target: self, action: #selector(UIViewController.urlPressed))
 		let photoButton = UIBarButtonItem(title: "Photo", style: UIBarButtonItemStyle.Plain, target: self, action: #selector(UIViewController.photoPressed))
 		let postButton = UIBarButtonItem(title: "Post", style: UIBarButtonItemStyle.Done, target: self, action: #selector(UIViewController.postPressed))
 		let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
-		toolBar.setItems([globeButton, spaceButton, urlButton, spaceButton, photoButton, spaceButton, postButton], animated: false)
+//		toolBar.setItems([globeButton, spaceButton, urlButton, spaceButton, photoButton, spaceButton, postButton], animated: false)
+		toolBar.setItems([spaceButton, urlButton, spaceButton, photoButton, spaceButton, postButton], animated: false)
 		toolBar.userInteractionEnabled = true
 		toolBar.sizeToFit()
 		
