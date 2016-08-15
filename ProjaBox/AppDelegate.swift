a57c1b90a977c88cc045ca9afb80bb9047e60d9f
alexclp <alexandru.clapa@gmail.com>
2016-08-15 22:28:56 +0300

    fixed iqkeyboardmanager permissions

--- ProjaBox/AppDelegate.swift
+++ ProjaBox/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
 		IQKeyboardManager.sharedManager().enable = true
-		IQKeyboardManager.sharedManager().disabledDistanceHandlingClasses.insert("CreatingPostViewController")
+		IQKeyboardManager.sharedManager().disabledDistanceHandlingClasses.insert(NSStringFromClass(CreatingPostViewController))
 		
 		return true
 	}
